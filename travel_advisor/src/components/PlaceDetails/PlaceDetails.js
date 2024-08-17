508fa4c06e1d7d11741a3febd5fabe3fffdffc48
KarunaShrestha02 <karuna100502@gmail.com>
2024-08-17 18:49:21 +0545

    no message

--- travel_advisor/src/components/PlaceDetails/PlaceDetails.js
+++ travel_advisor/src/components/PlaceDetails/PlaceDetails.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
-import PhoneIcon from '@material-ui/icons/Phone';
+import LocationOnIcon from '@material-ui/icons/LocationOnIcon';
+import PhoneIcon from '@material-ui/icons/PhoneIcon';
 import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles.js';
